9d1464eda23ba451a7c2d8763cea0836617c6503
Your Name <cbergenhem@gmail.com>
2013-01-15 23:40:05 -0500

    Add nav item animation for scroll and click

--- personal-page/scripts/site.js
+++ personal-page/scripts/site.js
@@ -1,58 +1,80 @@
 window.MyApp = (function($){
-	var _introduction, _about, _portfolio, _contact;
+	var _introduction, _about, _portfolio, _contact, _scrollTop;
+	var _positions = {};
 
 	function _calculatePositions(){
 		_introduction = 0;
-		_about = $('section#about').position().top;
-		_portfolio = $('section#portfolio').position().top;
-		_contact = $('section#contact').position().top;
+		_about = $('section#about').position().top - 40;
+		_portfolio = $('section#portfolio').position().top - 40;
+		_contact = $('section#contact').position().top - 40;
 	}
 
-    function _populatePositions(positions){
-      positions.introduction = _introduction;
-      positions.about = _about;
-      positions.portfolio = _portfolio;
-      positions.contact = _contact;
+    function _populatePositions(){
+      _positions.introduction = _introduction;
+      _positions.about = _about;
+      _positions.portfolio = _portfolio;
+      _positions.contact = _contact;
+    }
+
+    function _setScrollTop(){
+    	_scrollTop = $(window).scrollTop();
+    }
+
+    function _onClick(e){
+    	e.preventDefault();
+    	$('html, body').animate({
+    		scrollTop: _positions[$(this).attr('href')]
+    	}, 'slow');
+
+    	$(this).addClass('selected');
+    }
+
+    function _onScroll(e){
+    	_removeSelected();
+    	_setScrollTop();
+		if(_scrollTop > 255 && !$('nav').hasClass('pinned')){
+			$('nav').addClass('pinned');
+		}
+
+		if(_scrollTop <= 255 && $('nav').hasClass('pinned')){
+			$('nav').removeClass('pinned');
+		}
+
+		if(_scrollTop < _positions.about){
+			$('a[href=introduction]').addClass('selected');
+		}
+		else if(_scrollTop >= _positions.about && _scrollTop < _positions.portfolio){
+			$('a[href=about]').addClass('selected');
+		}
+		else if(_scrollTop >= _positions.portfolio && _scrollTop < _positions.contact){
+			$('a[href=portfolio]').addClass('selected');
+		}
+		else if(_scrollTop >= _positions.contact){
+			$('a[href=contact]').addClass('selected');
+		}
+    }
+
+    function _removeSelected(){
+    	var currentSelected = $('nav a.selected');
+    	if(currentSelected){
+    		currentSelected.removeClass('selected');
+    	}
+    }
+
+    function _eventBinder() {
+    	$('nav a').on('click', _onClick);
+    	$(window).on('scroll', _onScroll);
     }
 
 	return { 
 		init: function(){
 			_calculatePositions()
-		},
-		getPositions: function(){
-			var positions = { };
-			_populatePositions(positions);
-			return positions;
+			_populatePositions();
+			_eventBinder();
 		}
-
 	};
 })(jQuery);
 
 $(function() {
 	MyApp.init();
-
-	var sections = MyApp.getPositions();
-
-	$(window).scroll(function (){
-		scrollTop = $(window).scrollTop();
-
-		if(scrollTop > 255 && !$('nav').hasClass('pinned')){
-			$('nav').addClass('pinned');
-		}
-
-		if(scrollTop <= 255 && $('nav').hasClass('pinned')){
-			$('nav').removeClass('pinned');
-		}
-
-		if(scrollTop < sections.about){
-		}
-		else if(scrollTop >= sections.about && scrollTop < sections.portfolio){
-			$('nav').add
-		}
-		else if(scrollTop >= sections.portfolio && scrollTop < sections.contact){
-		}
-		else if(scrollTop >= sections.contact){
-		}
-
-	});
 });
